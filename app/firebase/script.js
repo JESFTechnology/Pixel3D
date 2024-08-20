a7d944504945f0c718cff05b5bb2cf8b5d9baaf1
JESF Technology <116103419+JESFTechnology@users.noreply.github.com>
2024-08-20 16:38:00 -0300

    Update script.js

--- app/firebase/script.js
+++ app/firebase/script.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/fireba
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
 import { getAuth, signInWithPopup, signInWithEmailAndPassword, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore , collection, getDocs, query } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+import { getDatabase, ref, set, onValue, remove } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 const firebaseConfig = {
     apiKey: "AIzaSyAKFHeOlxxbia66vTYJAdYKiJKO216BgvY",
     authDomain: "game-online-ifmachado.firebaseapp.com",
@@ -23,6 +23,13 @@ auth.languageCode = 'pt';
 provider.addScope('');
 
 let tela = 7;
+
+if(localStorage.getItem("tela") == null || localStorage.getItem("tela") == undefined){
+  tela = 7;
+}else{
+  tela = parseInt(localStorage.getItem("tela"));
+}
+
 
 const menu = document.getElementById("menu");
 
@@ -48,8 +55,86 @@ if(valorAleatorio1 < 300 || valorAleatorio1 > window.innerWidth-100){
     }
 }
 
-//console.log(valorAleatorio2)
+const inventario = []
+const itens_no_chao = []
 
+function tirarDoChao(nome){
+  remove(ref(db, `itens/${nome}`))
+}
+
+function addInventario(id, nome, img){
+  console.log("Colocando itens...")
+  if(id == 0){
+    id = inventario.length+1
+    //alert(inventario.length+1)
+  }
+  if(inventario.length < 4){
+    inventario.push({id:id, nome:nome, img:img})
+    console.log(inventario.length)
+    updateInventario()
+  }
+}
+
+function updateInventario(){
+  let slot = inventario.length
+    for(let i = 1; i <= slot; i++){
+        try{
+          set(ref(db, "inventario/"+localStorage.getItem("uid")+`/${i}`), {
+            item: inventario[i-1].nome,
+            img: inventario[i-1].img,
+          });
+        }catch (e){
+          console.log("Erro ao atualizar: ",e)
+        }
+    }
+  atualizarTela()
+}
+
+function largarInventario(valor){
+  console.log("Limpando inventario")
+    set(ref(db, "itens/"+inventario[valor-1].nome), {
+      x: (parseInt(localStorage.getItem("x"))+35),
+      y: (parseInt(localStorage.getItem("y"))+240),
+      img: inventario[valor-1].img,
+      tela: tela,
+      class: "item_de_mao"
+    });
+    remove(ref(db, `inventario/${localStorage.getItem("uid")}/${valor}`))
+    let index = inventario.findIndex((element) => element.id === valor);
+    if (index !== -1) {
+      inventario.splice(index, 1);
+    } else {
+      console.log(`Slot ${3} está vazio`);
+    }
+    updateInventario()
+}
+
+function atualizarTela(){
+  console.log("Atualizando itens...")
+  let slot = inventario.length
+  for(let i = 1; i <= slot; i++){
+    console.log("Atualizando inventario: "+slot)
+    const div = document.getElementById(`item-menu-${i}`)
+    try{
+      const imagem = document.getElementById('img'+i)
+      imagem.className = "ovo"
+      imagem.src = inventario[i-1].img
+    }catch{
+      const imagem = document.createElement('img')
+      imagem.id = "img"+i
+      imagem.className = "ovo"
+      imagem.src = inventario[i-1].img
+      div.append(imagem)
+    }
+  }
+}
+
+//console.log(valorAleatorio2)
+function gerarValorAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//const nascimentoOvo = gerarValorAleatorio(10, 1385);
 //const lanterna = document.getElementById("lanterna");
 
 //lanterna.style.opacity = 100
@@ -68,6 +153,8 @@ const fotoEstudante = document.getElementById("fotoEstudante")
 const mapa = document.getElementById("mapa")
 
 function colocarPlayers(){
+  console.log("Colocando personagens")
+  
   const dbRef2 = ref(db, 'itens');
 
   onValue(dbRef2, (snapshot) => {
@@ -88,7 +175,12 @@ function colocarPlayers(){
           const img = document.createElement("img")
           img.id = childKey;
           img.src = `${childData.img}`
-          img.style.width = "200px"
+          if(childData.class == "item_de_mao"){
+            img.style.width = "35px"
+            itens_no_chao.push({nome:childKey, tela:childData.tela, x:childData.x,y:childData.y,img:childData.img })
+          }else{
+            img.style.width = "200px"
+          }
           img.style.position = "absolute" 
           img.style.top = `${childData.y}px`;
           img.style.left = `${childData.x}px`;
@@ -104,7 +196,7 @@ function colocarPlayers(){
     });
   });
 
-  const dbRef = ref(db, 'users');
+  const dbRef = ref(db, 'users'); 
 
   onValue(dbRef, (snapshot) => {
     snapshot.forEach((childSnapshot) => {
@@ -119,32 +211,23 @@ function colocarPlayers(){
           img.style.opacity = 100
           img.style.top = `${childData.y+200}px`;
           img.style.left = `${childData.x}px`;
-          try{
-            if(childData.img != null || childData.img!="undefined"){
+            if(childData.img != undefined){
               img.src = childData.img
             }else{
-              img.src = "https://i.pinimg.com/originals/1c/04/48/1c0448350625530d3873a100248540d9.gif"
+              img.src = "https://art.pixilart.com/e00cff2d5f78554.png"
             } 
-          }catch{
-            img.src = "https://i.pinimg.com/originals/1c/04/48/1c0448350625530d3873a100248540d9.gif"
-          }
           //console.log(childData)
         }
       }catch (e){
         if(localStorage.getItem("uid") != childSnapshot.key){
           const childKey = childSnapshot.key;
-          //console.log(childKey)
           const img = document.createElement("img")
           img.id = childKey;
-          try{
-            if(childData.img != null || childData.img!="undefined"){
+          if(childData.img != undefined){
               img.src = childData.img
-            }else{
-              img.src = "https://i.pinimg.com/originals/1c/04/48/1c0448350625530d3873a100248540d9.gif"
-            } 
-          }catch{
-            img.src = "https://i.pinimg.com/originals/1c/04/48/1c0448350625530d3873a100248540d9.gif"
-          }
+          }else{
+              img.src = "https://art.pixilart.com/e00cff2d5f78554.png"
+          } 
           img.style.position = "absolute" 
           img.style.top = `${childData.y+200}px`;
           img.style.left = `${childData.x}px`;
@@ -191,7 +274,34 @@ function colocarPlayers(){
     }
     });
   });
+
+  const inventarioDB = ref(db, 'inventario/'+localStorage.getItem("uid"));
+
+  onValue(inventarioDB, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+    const childData = childSnapshot.val();
+    const childKey = childSnapshot.key;
+    if (parseInt(childKey) > inventario.length && parseInt(childKey) < 6){
+      let index = inventario.findIndex((element) => element.id === parseInt(childKey));
+      if (index !== -1) {
+        //console.log("Item "+childKey+" adicionado")
+      } else {
+        console.log("Item "+childKey+" adicionado")
+        addInventario(parseInt(childKey), childData.item, childData.img)
+        /*remove(ref(db, `inventario/${localStorage.getItem("uid")}/${childKey}`))
+        set(ref(db, `inventario/${localStorage.getItem("uid")}/${inventario.length}`), {
+          img: inventario[valor-1].img,
+          iten: childData.item
+        });*/
+      }
+    }
+    });
+  });
+  trocarTela()
+  updateInventario()
 }
+
+console.log("Online");
 
 function trocarTela(){
   alert_text.textContent = ""
@@ -222,7 +332,7 @@ function trocarTela(){
     mapa.style.backgroundImage = 'url(img/Laboratorio-2.png)'
   }
   localStorage.setItem("tela", tela)
-  colocarPlayers()
+  
 }
 
 function login(){
@@ -262,7 +372,8 @@ colocarPlayers()
 
 login()
 
-function atualizarCoord(userId, x, y, tela, imgPerso) {
+function atualizarCoord(userId, x, y, tela, imgPerso
+) {
     set(ref(db, "users/"+userId), {
       x: x,
       y: y,
@@ -321,12 +432,14 @@ window.addEventListener("keydown", function(event) {
                 xPos = 50;
                 image.style.left = `${xPos}px`;
                 trocarTela()
+                colocarPlayers()
               }
               else if(xPos >= 10 && xPos <= 60 && yPos <= 100 && yPos >= 0 && (tela == 17)){
                 tela = 7
                 xPos = 690;
                 image.style.left = `${xPos}px`;
                 trocarTela()
+                colocarPlayers()
               }else if(xPos >= 1000 && xPos <= 1160 && yPos <= 100 && yPos >= 0 && (tela == 8)){
                 tela = 18;
                 xPos = 60;
@@ -334,6 +447,7 @@ window.addEventListener("keydown", function(event) {
                 image.style.left = `${xPos}px`;
                 image.style.top = `${yPos}px`;
                 trocarTela()
+                colocarPlayers()
                }else if(xPos >= 10 && xPos <= 60 && yPos <= 100 && yPos >= 0 && (tela == 18)){
                 tela = 8
                 xPos = 1080;
@@ -341,9 +455,39 @@ window.addEventListener("keydown", function(event) {
                 image.style.left = `${xPos}px`;
                 image.style.top = `${yPos}px`;
                 trocarTela()
-              }
+                colocarPlayers()
+                }/*else if(xPos >= nascimentoOvo-nascimentoOvo*0.2 && xPos <= nascimentoOvo+nascimentoOvo*0.1 && yPos <= 350 && yPos >= 310 && (tela == 7)){
+                  let slot = inventario.length
+                  if(slot > 4){
+                    alert("Inventario cheio!")
+                    return 0;
+                  }
+                  const img = document.getElementById("ovo")
+                  img.style.opacity = 0
+                  addInventario("ovo", "img/ovo.png")
+                }*/
               break;
-      }
+        case "1":
+          console.log("Retirando item "+inventario[0].nome)
+          largarInventario(1)
+          window.location.href = "index.html";
+          break;
+        case "2":
+          console.log("Retirando item "+inventario[1].nome)
+          largarInventario(2)
+          window.location.href = "index.html";
+          break;
+        case "3":
+          console.log("Retirando item "+inventario[2].nome)
+          largarInventario(3)
+          window.location.href = "index.html";
+          break;
+        case "4":
+          console.log("Retirando item "+inventario[3].nome)
+          largarInventario(4)
+          window.location.href = "index.html";
+          break;
+        }
     if(tela == 7 && xPos >= larguraTela-60){
         xPos = 10
         tela = 8
@@ -374,14 +518,31 @@ window.addEventListener("keydown", function(event) {
    }else if(xPos >= 10 && xPos <= 60 && yPos <= 100 && yPos >= 0 && (tela == 18)){
     alert_text.textContent = "Aperte E para sair."
     alerta.style.opacity = 100
-   }else{
+   }
+   /*else if(xPos >= nascimentoOvo-nascimentoOvo*0.2 && xPos <= nascimentoOvo+nascimentoOvo*0.1 && yPos <= 350 && yPos >= 310 && (tela == 7)){
+    alert_text.textContent = "Aperte E para pegar."
+    alerta.style.opacity = 100
+   }*/
+   else{
     alert_text.textContent = ""
     alerta.style.opacity = 0
    }
 
-    atualizarCoord(this.localStorage.getItem("uid"), xPos, yPos, tela, this.localStorage.getItem("imgPerso"));
+  atualizarCoord(this.localStorage.getItem("uid"), xPos, yPos, tela, this.localStorage.getItem("imgPerso"));
+  //console.log(xPos+35)
+  //console.log(yPos+250)
+  for(let i = 0; i < itens_no_chao.length; i++){
+    if(itens_no_chao[i].tela == tela && xPos+35 >= (itens_no_chao[i].x)-(itens_no_chao[i].x)*0.1 && xPos+35 <= (itens_no_chao[i].x)+(itens_no_chao[i].x)*0.1 && yPos+250 >= (itens_no_chao[i].y)-(itens_no_chao[i].y)*0.1 && yPos+250 <= (itens_no_chao[i].y)+(itens_no_chao[i].y)*0.1){
+      alert_text.textContent = "Aperte E para pegar "+itens_no_chao[i].nome
+      alerta.style.opacity = 100
+      if(event.key == "e"){
+        addInventario(0,`${itens_no_chao[i].nome}`,`${itens_no_chao[i].img}`)
+        tirarDoChao(`${itens_no_chao[i].nome}`)
+        window.location.href = "index.html";
+      }
     }
-    catch{
+    }
+  }catch{
 
     }
   }); 
